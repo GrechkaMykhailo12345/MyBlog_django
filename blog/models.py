5837502723c53975a3a1c3a4ba4873b4bd766ac8
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-05-20 20:36:16 +0300

    Додав дані про Author в models py

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-from django.utils import timezone
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    published_date = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=100)
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return super().__str__()
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return super().__str__()
